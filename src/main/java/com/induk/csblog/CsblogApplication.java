1acc6596d626e7ae156330e008896314d341fd44
kjg <tyqq12@naver.com>
2021-10-18 15:27:30 +0900

    프로젝트 파일 생성

---
+++ src/main/java/com/induk/csblog/CsblogApplication.java
@@ -0,0 +1,13 @@
+package com.induk.csblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/induk/csblog/CsblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.induk.csblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

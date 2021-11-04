7193920739e5973ae6a7e1d386004fc38db76d6e
kimjw0010 <kimjw0012@gmail.com>
2021-11-04 12:13:31 +0900

    회원가입 시 패스워드 패턴 추가

--- src/main/java/com/induk/csblog/dto/JoinForm.java
+++ src/main/java/com/induk/csblog/dto/JoinForm.java
@@ -5,6 +5,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 @Data
 public class JoinForm {
@@ -12,6 +13,7 @@ public class JoinForm {
     @NotEmpty(message = "아이디를 입력해주세요")
     private String uid;
 
+    @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}", message = "비밀번호는 영어와 숫자, 특수문자로를 포함해서 만들어주세요.")
     @NotEmpty(message = "비밀번호를 입력해주세요")
     private String pw;
 
